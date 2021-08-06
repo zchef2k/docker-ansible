f72bd28c17145e1d23f6282dd4f918c037b18aa3
Zac Durham <zdurham@glenraven.com>
2021-08-06 16:37:42 -0400

    added rsa public key

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@
 echo "$ANSIBLE_PASSWORD" | passwd --stdin ansible
 
 # kubernetes mounted secrets have bad ownership and perms. fix that here:
-cat /opt/ansible/temp/id_rsa >> /opt/ansible/.ssh/id_rsa 
-chown ansible:ansible /opt/ansible/.ssh/id_rsa
-chmod 600 /opt/ansible/.ssh/id_rsa
+cat /opt/ansible/temp/id_rsa >> /opt/ansible/.ssh/id_rsa
+cat /opt/ansible/temp/id_rsa.pub >> /opt/ansible/.ssh/id_rsa.pub
+chown ansible:ansible /opt/ansible/.ssh/id_rsa*
+chmod 600 /opt/ansible/.ssh/id_rsa*
 
 # start sshd
 /usr/sbin/sshd -D
