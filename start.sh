1d699460118115d85a401ba8195bc9a69e465e1c
Zac Durham <zdurham@glenraven.com>
2021-08-06 16:51:10 -0400

    copy public key to authorized_keys

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "$ANSIBLE_PASSWORD" | passwd --stdin ansible
 
 # kubernetes mounted secrets have bad ownership and perms. fix that here:
 cat /opt/ansible/temp/id_rsa >> /opt/ansible/.ssh/id_rsa
-cat /opt/ansible/temp/id_rsa.pub >> /opt/ansible/.ssh/id_rsa.pub
+cat /opt/ansible/temp/id_rsa.pub >> /opt/ansible/.ssh/authorized_keys
 chown ansible:ansible /opt/ansible/.ssh/id_rsa*
 chmod 600 /opt/ansible/.ssh/id_rsa*
 
