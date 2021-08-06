60dda693435837fe896641f99312e90be463d1ea
Zac Durham <zdurham@glenraven.com>
2021-08-06 17:04:48 -0400

    more games with mount location of key files

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 echo "$ANSIBLE_PASSWORD" | passwd --stdin ansible
 
 # kubernetes mounted secrets have bad ownership and perms. fix that here:
-cat /opt/ansible/temp/id_rsa >> /opt/ansible/.ssh/id_rsa
-cat /opt/ansible/temp/id_rsa.pub >> /opt/ansible/.ssh/authorized_keys
-chown ansible:ansible /opt/ansible/.ssh/id_rsa*
-chmod 600 /opt/ansible/.ssh/id_rsa*
+cat /opt/ansible/temp-priv/id_rsa >> /opt/ansible/.ssh/id_rsa
+cat /opt/ansible/temp-pub/id_rsa.pub >> /opt/ansible/.ssh/authorized_keys
+chown ansible:ansible /opt/ansible/.ssh/*
+chmod 600 /opt/ansible/.ssh/*
 
 # start sshd
 /usr/sbin/sshd -D
