5e03fb5d8a5008e7fb47d723dc011c73c7e92ebc
Zac Durham <zdurham@glenraven.com>
2021-08-06 17:22:14 -0400

    refactored ssh key process

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@
 echo "$ANSIBLE_PASSWORD" | passwd --stdin ansible
 
 # kubernetes mounted secrets have bad ownership and perms. fix that here:
-cat /opt/ansible/temp-priv/id_rsa >> /opt/ansible/.ssh/id_rsa
-cat /opt/ansible/temp-pub/id_rsa.pub >> /opt/ansible/.ssh/authorized_keys
+cat /opt/ansible/temp/id_ed25519 >> /opt/ansible/.ssh/id_ed25519
+cat /opt/ansible/temp/authorized_keys >> /opt/ansible/.ssh/authorized_keys
 chown ansible:ansible /opt/ansible/.ssh/*
 chmod 600 /opt/ansible/.ssh/*
 
