f5c4af5779306125aa34489a970608284257efc1
Zac Durham <zdurham@glenraven.com>
2021-08-06 13:54:46 -0400

    fix typos in last commit. change github action to build on merge to 'release'

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # change ansible's password
 echo "$ANSIBLE_PASSWORD" | passwd --stdin ansible
 
-# kubernetes mounted secrets have bad ownership and perms. fix that here
-cat /opt/ansbile/.ssh/id_rsa_temp /opt/ansbile/.ssh/id_rsa 
-chown ansbile:ansbile /opt/ansbile/.ssh/id_rsa
+# kubernetes mounted secrets have bad ownership and perms. fix that here:
+cat /opt/ansible/.ssh/id_rsa_temp /opt/ansible/.ssh/id_rsa 
+chown ansible:ansible /opt/ansible/.ssh/id_rsa
 chmod 600 /opt/ansible/.ssh/id_rsa
 
 # start sshd
